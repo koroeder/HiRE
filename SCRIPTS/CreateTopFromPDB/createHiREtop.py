7c9281d27e2907ebcfa09d9db1a49ac44d0ddb93
kr366 <kr366@cam.ac.uk>
2022-05-31 12:30:50 +0100

    Further funcitonality in top level script

--- SCRIPTS/CreateTopFromPDB/createHiREtop.py
+++ SCRIPTS/CreateTopFromPDB/createHiREtop.py
@@ -1,8 +1,19 @@
 import PDBhandler
 import FA2CGmapper
+import topology
 import sys
 
 data = PDBhandler.parse_pdb(sys.argv[1])
 natom,nres,atomnames,elements,res,resnames,coordsbyres,termini = data
+atommasses = FA2CGmapper.assign_atommass(atomnames)
 CG_labels,CG_coords,CG_resrange = FA2CGmapper.get_CG_info(resnames,res,atomnames,atommasses,coordsbyres)
+CG_masses = FA2CGmapper.assign_CGmasses(CG_labels)
+CG_termini = FA2CGmapper.get_CG_termini(res,termini,CG_resrange)
 
+top_dict = topology.init_topology()
+top_dict["nres"] = nres
+top_dict["nparticles"] = len(CG_labels)
+top_dict["nmol"] = len(CG_termini)/2
+top_dict["fl_mol"] = CG_termini
+top_dict["particle_names"] = CG_labels
+top_dict["res_names"] = list()
