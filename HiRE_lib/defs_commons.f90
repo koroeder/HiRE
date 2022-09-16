fbe07de0ad175de4ebd61e83eb0ebce0cf133234
Dr K. Roeder <kr366@cam.ac.uk>
2022-09-16 16:28:44 +0100

    Added documentation

--- HiRE_lib/defs_commons.f90
+++ HiRE_lib/defs_commons.f90
@@ -1,45 +1,69 @@
-!Defined precision kinds
+!> @file
+!> Contains modules with global variables (PREC_HIRE, VAR_DEFS, VAR_UTILS, SAXS_DEFS, HB_DEFS and NUM_DEFS)
+
+!> Defined precision kinds
 MODULE PREC_HIRE
+  !> integer 32-bit
   INTEGER, PARAMETER  :: INT32  = SELECTED_INT_KIND(9)
+  !> integer 64-bit  
   INTEGER, PARAMETER  :: INT64  = SELECTED_INT_KIND(18)
+  !> real, double precision 32-bit
   INTEGER, PARAMETER  :: REAL32 = SELECTED_REAL_KIND(6, 37)
+  !> real, double precision 64-bit  
   INTEGER, PARAMETER  :: REAL64 = SELECTED_REAL_KIND(15, 307)  
 END MODULE PREC_HIRE
 
-!Global variables
+!> Global variables for HiRE
 MODULE VAR_DEFS
   USE PREC_HIRE, ONLY: REAL64
   IMPLICIT NONE
   
-  INTEGER :: NRES       !number of residues
-  INTEGER :: NPARTICLES !number of CG particles 
-  INTEGER :: NOPT       !number of degrees of freedom (3*NPARTICLES)
-  INTEGER :: NTYPEP     !number of different particle types
-  INTEGER :: NCHAINS    !number of different particle types
+  !> number of residues
+  INTEGER :: NRES
+  !> number of CG particles
+  INTEGER :: NPARTICLES 
+  !> number of degrees of freedom (3*NPARTICLES)
+  INTEGER :: NOPT       
+  !> number of different particle types
+  INTEGER :: NTYPEP     
+  !> number of different chains (molecules)
+  INTEGER :: NCHAINS    
   
-  REAL(KIND = REAL64), ALLOCATABLE :: CHATM(:)  !Charge of atoms in simulation
-  REAL(KIND = REAL64), ALLOCATABLE :: AMASS(:)  !Masses of the beads
-  INTEGER, ALLOCATABLE :: IAC(:)                !Particle ids
-  INTEGER, ALLOCATABLE :: RESSTART(:)           !Index of first particle in res
-  INTEGER, ALLOCATABLE :: RESFINAL(:)           !Index of final particle in res
-  INTEGER, ALLOCATABLE :: RESTYPES(:)           !Type of residue 0-RNA, 1-DNA, 2-protein, 3-other
-  INTEGER, ALLOCATABLE :: FRAG_PAR_PTR(:,:)     !First and last particle for each chain
-  INTEGER, ALLOCATABLE :: FRAG_RES_PTR(:,:)     !First and last residue for each chain  
-  INTEGER, ALLOCATABLE :: RESSIZE(:)            !Number of particles of each residue
-  INTEGER, ALLOCATABLE :: FRAGSIZE(:)           !Number of particles per fragment
-    
-  CHARACTER(4), ALLOCATABLE :: IGRAPH(:)        !Particle names
-  CHARACTER(4), ALLOCATABLE :: RESNAMES(:)      !Residue name
-  
+  !> List of charges of all particles
+  REAL(KIND = REAL64), ALLOCATABLE :: CHATM(:)  
+  !> List of the masses of the particles
+  REAL(KIND = REAL64), ALLOCATABLE :: AMASS(:)  
+  !> List of particle ids
+  INTEGER, ALLOCATABLE :: IAC(:)                
+  !> List of indices of first particle in residues
+  INTEGER, ALLOCATABLE :: RESSTART(:)           
+  !> List of indices of final particle in residues
+  INTEGER, ALLOCATABLE :: RESFINAL(:)           
+  !> List of types of residue 0-RNA, 1-DNA, 2-protein, 3-other
+  INTEGER, ALLOCATABLE :: RESTYPES(:)          
+  !> First and last particle for each chain
+  INTEGER, ALLOCATABLE :: FRAG_PAR_PTR(:,:)     
+  !> First and last residue for each chain 
+  INTEGER, ALLOCATABLE :: FRAG_RES_PTR(:,:)     
+  !> Number of particles of each residue
+  INTEGER, ALLOCATABLE :: RESSIZE(:)            
+  !> Number of particles per fragment (molecule)
+  INTEGER, ALLOCATABLE :: FRAGSIZE(:)           
+
+  !> List of particle names   
+  CHARACTER(4), ALLOCATABLE :: IGRAPH(:)        
+  !> List of residue name
+  CHARACTER(4), ALLOCATABLE :: RESNAMES(:)      
 END MODULE VAR_DEFS
 
+!> Utilities for variables in VAR_DEFS
 MODULE VAR_UTILS
   USE PREC_HIRE, ONLY: REAL64
   USE VAR_DEFS 
   IMPLICIT NONE
 
   CONTAINS 
-    !Allocation of some generic arrays
+    !> Allocation for arrays in VAR_DEFS
     SUBROUTINE ALLOC_VARS()
        CALL DEALLOC_VARS()
        ALLOCATE(AMASS(NPARTICLES), CHATM(NPARTICLES), IAC(NPARTICLES), &
@@ -48,7 +72,7 @@ MODULE VAR_UTILS
                 FRAG_RES_PTR(NCHAINS,2), RESFINAL(NRES), RESTYPES(NRES))
     END SUBROUTINE ALLOC_VARS
     
-    !Deallocation of those arrays
+    !> Deallocation of the arrays in VAR_DEFS
     SUBROUTINE DEALLOC_VARS()
        IF (ALLOCATED(AMASS)) DEALLOCATE(AMASS)
        IF (ALLOCATED(CHATM)) DEALLOCATE(CHATM)
@@ -61,58 +85,87 @@ MODULE VAR_UTILS
        IF (ALLOCATED(RESFINAL)) DEALLOCATE(RESFINAL)
        IF (ALLOCATED(RESTYPES)) DEALLOCATE(RESTYPES)
        IF (ALLOCATED(RESNAMES)) DEALLOCATE(RESNAMES) 
-       IF (ALLOCATED(RESSIZE)) DEALLOCATE(RESSIZE)
-                    
+       IF (ALLOCATED(RESSIZE)) DEALLOCATE(RESSIZE)           
     END SUBROUTINE DEALLOC_VARS    
     
-
 END MODULE VAR_UTILS
 
+!> Module containing global variables for SAXS
 MODULE SAXS_DEFS
    USE PREC_HIRE, ONLY: REAL64
    IMPLICIT NONE 
    ! saxs serial parameters
    logical :: compute_SAXS_serial, SAXS_save, modulate_SAXS_serial
    real(kind = real64) :: SAXS_invsig
-   integer :: saxs_serial_step, SAXSs, SAXSc
+   integer :: saxs_serial_step
+   !> output unit
+   integer :: SAXSs
+   !> output unit
+   integer :: SAXSc
    integer :: n_rate_saxs
 
-   ! Booleans deciding if we should compute a SAXS forces next step and save SAXS data
+   !> Boolean deciding if we should compute a SAXS forces next step
    logical :: calc_SAXS_force = .true.
+   !> Boolean deciding if we should save SAXS data
    logical :: SAXS_print = .false.
   
-   LOGICAL           :: SAXST = .FALSE.           !use SAXS
-   INTEGER           :: SAXSNSTEPS = 1            !frequency for applying SAXS
-   LOGICAL           :: SAXSFORCET = .FALSE.      !compute SASX force at MC transition?
-   LOGICAL           :: SAXSMODULT = .FALSE.      !modulate SAXS force with decreasing and periodic factor?
-   REAL(KIND=REAL64) :: SAXSINVSIG = 2.5          !broadness of the periodic modulation
-   REAL(KIND=REAL64) :: SAXSWAVE = 100.0          !length of the decreasing modulation 
-   REAL(KIND=REAL64) :: SAXSOFFI = 0.0D0          !step variable for periodic modulation
-   REAL(KIND=REAL64) :: SAXSMODI = 1.0D0          !step variable for decreasing modulation
-   LOGICAL           :: SAXSPRINT = .FALSE.       !print curves and scores to unit
-   INTEGER           :: SAXSNPRINT = 100          !frequency for printing SAXS
-   LOGICAL           :: SAXSSAVET  = .FALSE.      !save computed SAXS at MC transition?
-   REAL(KIND=REAL64) :: SAXSMAX = 1.0             !max value for SAXS vector
-   LOGICAL           :: SAXSSOLT = .FALSE.        !vacuum (false) or solution (true) calculation
-   LOGICAL           :: REFINET = .FALSE.         !refine hydration
-   INTEGER           :: NWATLAY = 1               !number of hydration layers
-   REAL(KIND=REAL64) :: WATRAD = 2.55             !water radius
-   REAL(KIND=REAL64) :: WATW = 0.037              !water contrast
+   !> use SAXS
+   LOGICAL           :: SAXST = .FALSE.           
+   !> frequency for applying SAXS
+   INTEGER           :: SAXSNSTEPS = 1            
+   !> compute SASX force at MC transition?
+   LOGICAL           :: SAXSFORCET = .FALSE.      
+   !> modulate SAXS force with decreasing and periodic factor?
+   LOGICAL           :: SAXSMODULT = .FALSE.      
+   !> broadness of the periodic modulation
+   REAL(KIND=REAL64) :: SAXSINVSIG = 2.5          
+   !> length of the decreasing modulation
+   REAL(KIND=REAL64) :: SAXSWAVE = 100.0           
+   !> step variable for periodic modulation
+   REAL(KIND=REAL64) :: SAXSOFFI = 0.0D0          
+   !> step variable for decreasing modulation
+   REAL(KIND=REAL64) :: SAXSMODI = 1.0D0          
+   !> print curves and scores to unit
+   LOGICAL           :: SAXSPRINT = .FALSE.       
+   !> frequency for printing SAXS
+   INTEGER           :: SAXSNPRINT = 100          
+   !> save computed SAXS at MC transition?
+   LOGICAL           :: SAXSSAVET  = .FALSE.      
+   !> max value for SAXS vector
+   REAL(KIND=REAL64) :: SAXSMAX = 1.0             
+   !> vacuum (false) or solution (true) calculation
+   LOGICAL           :: SAXSSOLT = .FALSE.        
+   !> refine hydration
+   LOGICAL           :: REFINET = .FALSE.         
+   !> number of hydration layers
+   INTEGER           :: NWATLAY = 1               
+   !> water radius
+   REAL(KIND=REAL64) :: WATRAD = 2.55             
+   !> water contrast
+   REAL(KIND=REAL64) :: WATW = 0.037              
 END MODULE SAXS_DEFS
 
+!> Module with the hydrogen bonding global variables
 MODULE HB_DEFS
+   !> Number of hydrogen-bonds
    INTEGER :: IHB = 0
+   !> Is H-bonding initialised?
    LOGICAL :: DO_HB = .FALSE.
+   !> Shall the information be saved?
    LOGICAL :: SAVE_HB = .FALSE.
+   !> Unit for output file
    INTEGER :: HBDAT
 END MODULE HB_DEFS
 
+!> numerical definitions
 MODULE NUM_DEFS
   USE PREC_HIRE, ONLY: REAL64
   IMPLICIT NONE  
-  
+  !> value of pi
   REAL(KIND = REAL64), PARAMETER :: PI = 3.141592653589793D0
+  !> radian to degree transformation
   REAL(KIND = REAL64), PARAMETER :: RAD2DEG = 57.29577951308232088D0
+  !> radian to degree transformation squared
   REAL(KIND = REAL64), PARAMETER :: RAD2 = RAD2DEG * RAD2DEG
 END MODULE NUM_DEFS
 
