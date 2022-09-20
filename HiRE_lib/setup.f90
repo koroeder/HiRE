8dd44a0fbd58d1e26be917e3d4b83e5e63425b18
Dr K. Roeder <kr366@cam.ac.uk>
2022-09-20 15:22:30 +0100

    Added documentation

--- HiRE_lib/setup.f90
+++ HiRE_lib/setup.f90
@@ -1,3 +1,16 @@
+!> @file
+!> Contains setup routine for HiRE
+
+!> Setup for HiRE
+!> @brief
+!>
+!> Routine is setting up the HiRE interface\n
+!> First the topology and the scale.dat file are read.\n
+!> Then the various modules for energy contributions are initialised and the routine checks for restraints.\n
+!> Finally the parameter sets for bases and particles are set and filled.
+!>
+!> @param[in] TOPNAME - topology name
+!> @param[in] SCALEDATNAME - scale.dat file name
 SUBROUTINE SETUP(TOPNAME, SCALEDATNAME)
    USE MOD_INIT, ONLY: TOPOLOGY_INPUT, READ_SCALE_DAT, CREATE_CONSTR, &
                        INIT_FROM_MODS
