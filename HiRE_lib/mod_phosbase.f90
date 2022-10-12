fcf88d85e9bccbd923608edc8b3914f81b439258
Dr K. Roeder <kr366@cam.ac.uk>
2022-10-12 15:51:19 +0100

    New module for phosphate base interactions (empty)

---
+++ HiRE_lib/mod_phosbase.f90
@@ -0,0 +1,20 @@
+!> @file
+!> Contains MOD_PHOSBASE dealing with phosphate base interactions
+
+!> Module containing all routines and variables to calculate the phosphate-base interaction contributions to E and F\n
+!> 
+MODULE MOD_BONDS
+   USE PREC_HIRE
+   !> Put module variables here              
+
+   CONTAINS
+
+
+      !> Calculate the energy and force contribution from the phosphate-base interactions
+      SUBROUTINE ENERGY_PHOSBASE(NOPT, X, F, EBOND)
+
+
+      END SUBROUTINE ENERGY_PHOSBASE
+
+
+END MODULE MOD_BONDS
