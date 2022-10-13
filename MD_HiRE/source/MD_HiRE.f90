146ad2d205dc49e55bda2c86bbd90f7d61254234
Dr K. Roeder <kr366@cam.ac.uk>
2022-10-13 12:22:44 +0100

    Modules for MD engine to be written

---
+++ MD_HiRE/source/MD_HiRE.f90
@@ -0,0 +1,14 @@
+! MD engine for HiRE
+
+!> @author 
+!> Dr Konstantin Roeder, University of Cambridge, 2022
+!>
+!> @file
+!> Main program to run MD simulation with HiRE force field
+PROGRAM MD_HIRE
+   USE MD_SETUP
+   USE MD_SIMULATION
+   USE MD_FINAL
+   
+
+END PROGRAM MD_HIRE
