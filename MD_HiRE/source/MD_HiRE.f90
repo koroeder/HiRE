4ffd752174f9de812fad03d68bf2fd423178d57d
kr366 <kr366@cam.ac.uk>
2022-11-01 17:08:00 +0000

    Started workflow for MD simulations

--- MD_HiRE/source/MD_HiRE.f90
+++ MD_HiRE/source/MD_HiRE.f90
@@ -6,9 +6,15 @@
 !> @file
 !> Main program to run MD simulation with HiRE force field
 PROGRAM MD_HIRE
-   USE MD_SETUP
+   USE MD_SETUP, ONLY: READ_SETTINGS, SETUP_POTENTIAL
    USE MD_SIMULATION
    USE MD_FINAL
+   USE MD_UTILS, ONLY: REPORT_PARAMS
    
+   ! 1. Initialise everything appropriately
+   CALL READ_SETTINGS()
+   CALL SETUP_POTENTIAL()
+
+   ! 2. Report the settings for the simulation to the output file
 
 END PROGRAM MD_HIRE
