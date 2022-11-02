9a5981d49d99fe7f6d24c5c72ddc792af149ea34
kr366 <kr366@cam.ac.uk>
2022-11-02 18:29:36 +0000

    Extended the work flow coded in main program, now runs up to the first step - next part of coding: DM steps

--- MD_HiRE/source/MD_HiRE.f90
+++ MD_HiRE/source/MD_HiRE.f90
@@ -6,15 +6,29 @@
 !> @file
 !> Main program to run MD simulation with HiRE force field
 PROGRAM MD_HIRE
+   USE MD_COMMONS, ONLY: MYUNIT
    USE MD_SETUP, ONLY: READ_SETTINGS, SETUP_POTENTIAL
    USE MD_SIMULATION
    USE MD_FINAL
-   USE MD_UTILS, ONLY: REPORT_PARAMS
+   USE MD_UTILS, ONLY: REPORT_PARAMS, MD_START, START_TRACKING
    
-   ! 1. Initialise everything appropriately
+   ! 1. Simulation setup
+   ! a) check the parameter input exists
+   CALL MD_START()
+   ! b) Read in all the simulation settings
    CALL READ_SETTINGS()
+   ! c) Initialise potential and get coordinates, mass and names from HiRE
    CALL SETUP_POTENTIAL()
+   ! c) Report the settings for the simulation to the output file
+   CALL REPORT_PARAMS()
+   ! d) Open tracking files
+   CALL START_TRACKING()
 
-   ! 2. Report the settings for the simulation to the output file
+   ! 2. Initialise velocities for simulation and get initial state
+   WRITE(MYUNIT,'(A)') ""
+   WRITE(MYUNIT,'(A)') " mdhire> Calling velocity initialisation"
+   CALL INITIALISE_VEL()
+   CALL ZERO_STEP()
 
+   ! 3. Run MD steps
 END PROGRAM MD_HIRE
