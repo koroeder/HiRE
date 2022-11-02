d83d8243318c5397bba0c80221626edf47ea179d
kr366 <kr366@cam.ac.uk>
2022-11-02 18:28:31 +0000

    New subroutine for zeroth step, i.e. gettting initial energy and acceleration

--- MD_HiRE/source/MD_simul.f90
+++ MD_HiRE/source/MD_simul.f90
@@ -1,3 +1,22 @@
 MODULE MD_SIMULATION
+   CONTAINS
+      SUBROUTINE ZERO_STEP()
+         USE MD_COMMONS, ONLY: NATOMS, MYUNIT, COORDS, EPOT, EKIN, ACC, MASSES
+         USE MD_UTILS, ONLY: SET_DERIVED_PARAMS
+         USE HIRE_INTERFACE, ONLY: HIRE_ENERGY_GRAD
+         IMPLICIT NONE
+         INTEGER :: I, J, IDX
+         ! set half step and friction params
+         CALL SET_DERIVED_PARAMS()
+         ! get initial energies
+         CALL HIRE_ENERGY_GRAD(3*NATOMS, COORDS, EPOT, ACC)
+         DO I=1,NATOMS
+            DO J=1,3
+               IDX = 3*(I-1) + J
+               ACC(IDX) = -ACC(IDX)/MASSES(I)
+            END DO
+         END DO
+         WRITE(MYUNIT,'(A)') " mdhire> Initial energies - EPOT= ", EPOT, "; EKIN= ", EKIN
+      END SUBROUTINE ZERO_STEP
 
 END MODULE MD_SIMULATION
