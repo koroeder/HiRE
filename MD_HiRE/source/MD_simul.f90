81a5f238e46d9f0866787a93598a05978417d670
Dr K. Roeder <kr366@cam.ac.uk>
2022-11-03 11:34:54 +0000

    Routines for taking MD step and dumping data

--- MD_HiRE/source/MD_simul.f90
+++ MD_HiRE/source/MD_simul.f90
@@ -19,4 +19,91 @@ MODULE MD_SIMULATION
          WRITE(MYUNIT,'(A)') " mdhire> Initial energies - EPOT= ", EPOT, "; EKIN= ", EKIN
       END SUBROUTINE ZERO_STEP
 
+      SUBROUTINE RUN_MD()
+         USE MD_COMMONS, ONLY: MDSTEPS
+         IMPLICIT NONE
+         INTEGER :: J
+
+         DO J=1,MDSTEPS
+            CALL TAKE_MDSTEP(J)
+            CALL DUMPDATA(J)
+         END DO
+      END SUBROUTINE RUN_MD
+
+      SUBROUTINE TAKE_MDSTEP(CURRSTEP)
+         USE MD_COMMONS, ONLY: MYUNIT, NDUMPE, HDT, DT, GAMMA, GFRIC, COORDS, VEL, ACC, MASSES, EKIN, EPOT 
+         USE RAND_ROUTINES, ONLY: RAND_NORMAL
+         USE HIRE_INTERFACE, ONLY: HIRE_ENERGY_GRAD
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: CURRSTEP
+         REAL(KIND = REAL64), PARAMETER :: PI = 3.141592653589793D0
+         REAL(KIND = REAL64) :: NR1, NR2
+         REAL(KIND = REAL64) :: NOISE(NATOMS)
+         INTEGER :: I, J, K, IDX
+         ! set noise to be used
+         DO I=1,NATOMS
+            NOISE(I) = DSQRT(TEMP*GAMMA*DT/MASSES(I))
+         END DO
+
+         DO I=1,NATOMS
+            DO J=1,3
+               IDX = 3*(I-1) + J
+               ! half-step velocity update
+               CALL RAND_NORMAL(1.0, 0.0, NR1)
+               VEL(IDX) = GFRIC*VEL(IDX) + ACC(IDX)*HDT + NR1*NOISE(I)
+               !update full-step coordinates
+               COORDS(IDX) = COORDS(IDX) + VEL(IDX)*DT
+            END DO
+         END DO
+         ! get new potential energy and gradient
+         CALL HIRE_ENERGY_GRAD(3*NATOMS, COORDS, EPOT, ACC)
+         EKIN=0.0D0
+         DO I=1,NATOMS
+            DO J=1,3
+               IDX = 3*(I-1) + J
+               ! update acceleration
+               ACC(IDX) = -ACC(IDX)/MASSES(I)
+               ! update full step velocity
+               CALL RAND_NORMAL(1.0, 0.0, NR2)
+               VEL(IDX) = GFRIC*VEL(IDX) + ACC(IDX)*HDT + NR2*NOISE(I)
+               ! get kinetic energy
+               EKIN = EKIN + MASSES(I)*VEL(IDX)*VEL(IDX)
+            END DO
+         END DO
+         EKIN = 0.5*EKIN
+         IF (MOD(CURRSTEP,NDUMPE).EQ.0) THEN
+            WRITE(MYUNIT,*) " mdsteps> Completed step ", CURRSTEP
+            WRITE(MYUNIT,*) "          Total energy: ", EPOT+EKIN           
+            WRITE(MYUNIT,*) "          Kinetic energy: ", EKIN
+            WRITE(MYUNIT,*) "          Potential energy: ", EPOT
+            WRITE(MYUNIT,*) " -----------------------------------------------"
+         END IF
+      END SUBROUTINE TAKE_MDSTEP
+
+      SUBROUTINE DUMPDATA(CURRSTEP)
+         USE MD_COMMONS, ONLY: NATOMS, XUNIT, EUNIT, DUMPPDBT, NDUMPE, NDUMPP, NDUMPX, COORDS, EKIN, EPOT
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: CURRSTEP
+         INTEGER :: I
+         CHARACTER(LEN=15) :: JSTRING
+         CHARACTER(LEN=30) :: PDBNAME
+         !write energies
+         WRITE(EUNIT,(I10,3(1X,F15.7))) CURRSTEP, EPOT+EKIN, EPOT, EKIN
+         !write coordinate files
+         IF (MOD(CURRSTEP,NDUMPX).EQ.0) THEN
+            WRITE(XUNIT,*) " Step: ", CURRSTEP
+            DO I=1,NATOMS
+               WRITE(XUNIT,'(3F15.7)') COORDS(3*I-2), COORDS(3*I-1), COORD(3*I)
+            END DO
+            WRITE(XUNIT,*) "-----------------------------------------------"
+         END IF
+         !write pdb
+         IF (DUMPPDBT) THEN
+            IF (MOD(CURRSTEP,NDUMPP).EQ.0) THEN
+               WRITE(JSTRING, '(I12.12)') CURRSTEP
+               PDBNAME = "mdx_"//ADJUSTL(TRIM(JSTRING))//".pdb"
+               CALL DUMP_PDB(3*NATOMS,COORDS,PDBNAME,.TRUE.)
+            END IF
+         END IF         
+      END SUBROUTINE DUMPDATA
 END MODULE MD_SIMULATION
