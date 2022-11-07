7edfa525d14fb87597713d5bc00c3cf7927b94c7
Dr K. Roeder <kr366@cam.ac.uk>
2022-11-07 10:37:14 +0000

    Corrected errors and little bugs

--- MD_HiRE/source/MD_simul.f90
+++ MD_HiRE/source/MD_simul.f90
@@ -31,7 +31,9 @@ MODULE MD_SIMULATION
       END SUBROUTINE RUN_MD
 
       SUBROUTINE TAKE_MDSTEP(CURRSTEP)
-         USE MD_COMMONS, ONLY: MYUNIT, NDUMPE, HDT, DT, GAMMA, GFRIC, COORDS, VEL, ACC, MASSES, EKIN, EPOT 
+         USE NUMKIND
+         USE MD_COMMONS, ONLY: MYUNIT, NATOMS, NDUMPE, HDT, DT, GAMMA, GFRIC, &
+                               COORDS, VEL, ACC, MASSES, EKIN, EPOT, TEMP 
          USE RAND_ROUTINES, ONLY: RAND_NORMAL
          USE HIRE_INTERFACE, ONLY: HIRE_ENERGY_GRAD
          IMPLICIT NONE
@@ -49,7 +51,7 @@ MODULE MD_SIMULATION
             DO J=1,3
                IDX = 3*(I-1) + J
                ! half-step velocity update
-               CALL RAND_NORMAL(1.0, 0.0, NR1)
+               CALL RAND_NORMAL(1.0D0, 0.0D0, NR1)
                VEL(IDX) = GFRIC*VEL(IDX) + ACC(IDX)*HDT + NR1*NOISE(I)
                !update full-step coordinates
                COORDS(IDX) = COORDS(IDX) + VEL(IDX)*DT
@@ -64,7 +66,7 @@ MODULE MD_SIMULATION
                ! update acceleration
                ACC(IDX) = -ACC(IDX)/MASSES(I)
                ! update full step velocity
-               CALL RAND_NORMAL(1.0, 0.0, NR2)
+               CALL RAND_NORMAL(1.0D0, 0.0D0, NR2)
                VEL(IDX) = GFRIC*VEL(IDX) + ACC(IDX)*HDT + NR2*NOISE(I)
                ! get kinetic energy
                EKIN = EKIN + MASSES(I)*VEL(IDX)*VEL(IDX)
@@ -82,18 +84,19 @@ MODULE MD_SIMULATION
 
       SUBROUTINE DUMPDATA(CURRSTEP)
          USE MD_COMMONS, ONLY: NATOMS, XUNIT, EUNIT, DUMPPDBT, NDUMPE, NDUMPP, NDUMPX, COORDS, EKIN, EPOT
+         USE HIRE_INTERFACE, ONLY: DUMP_PDB
          IMPLICIT NONE
          INTEGER, INTENT(IN) :: CURRSTEP
          INTEGER :: I
          CHARACTER(LEN=15) :: JSTRING
          CHARACTER(LEN=30) :: PDBNAME
          !write energies
-         WRITE(EUNIT,(I10,3(1X,F15.7))) CURRSTEP, EPOT+EKIN, EPOT, EKIN
+         WRITE(EUNIT,'(I10,3(1X,F15.7))') CURRSTEP, EPOT+EKIN, EPOT, EKIN
          !write coordinate files
          IF (MOD(CURRSTEP,NDUMPX).EQ.0) THEN
             WRITE(XUNIT,*) " Step: ", CURRSTEP
             DO I=1,NATOMS
-               WRITE(XUNIT,'(3F15.7)') COORDS(3*I-2), COORDS(3*I-1), COORD(3*I)
+               WRITE(XUNIT,'(3F15.7)') COORDS(3*I-2), COORDS(3*I-1), COORDS(3*I)
             END DO
             WRITE(XUNIT,*) "-----------------------------------------------"
          END IF
