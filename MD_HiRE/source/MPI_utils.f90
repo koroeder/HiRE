24f735d1f73ccee2c44bd6e2bfab7107a4c56f23
kr366 <kr366@cam.ac.uk>
2023-01-27 12:36:18 +0000

    New function to start tracking

--- MD_HiRE/source/MPI_utils.f90
+++ MD_HiRE/source/MPI_utils.f90
@@ -1,41 +1,54 @@
 MODULE MD_MPI
    USE NUMKIND
    IMPLICIT NONE
-   
+
    INTEGER :: ERR_CODE_MPI
    INTEGER :: REMD_TAG = 12
    CONTAINS
+
+      SUBROUTINE START_TRACKING_MPI()
+         USE FILE_UTILS, ONLY: FILE_OPEN
+         USE MD_COMMONS
+         IMPLICIT NONE
+         CHARACTER(LEN=3) :: IDSTR
+
+         WRITE(TASKID,'(I3)') IDSTR
+         CALL FILE_OPEN("md_energy."//ADJUSTL(TRIM(IDSTR))//".log",EUNIT,.TRUE.)
+         CALL FILE_OPEN("md_coords."//ADJUSTL(TRIM(IDSTR))//".xyz",XUNIT,.TRUE.)
+      END SUBROUTINE START_TRACKING_MPI
 
       SUBROUTINE COMMUNICATE_SETTINGS()
          USE MD_COMMONS
          IMPLICIT NONE
          REAL(KIND = REAL64), ALLOCATABLE :: TARRAY(:), STEP
+#ifdef MPI       
          INTEGER :: J
-         
-         
+         INCLUDE 'mpif.h'
+         INTEGER MPISTATUS(MPI_STATUS_SIZE)
+         INTEGER :: ERR_CODE_MPI, REMD_TAG
+
          IF(TASKID.EQ.0) THEN
             IF (NREPLICA.NE.NTASKS) THEN
-               WRITE(MYUNIT,'(A)') " comm_settings> WARNING - number of replica and number of tasks differ, &
+               WRITE(MYUNIT,'(A)') " comm_settings> WARNING  number of replica and number of tasks differ, &
                                      resetting them to be equal by changing the number of replicas"
                NREPLICA = NTASKS
             END IF
+            ALLOCATE(TARRAY(NREPLICA))
+            STEP = (HIGHR - LOWR)/DBLE(NREPLICA-1)
+            DO J=0,NREPLICA-1
+               TARRAY(J+1) = LOWR + J*STEP
+            END DO
          END IF
-         ! use BCAST for globals
 
          ! use SEND/RECV for temperature and lambda
-         ALLOCATE(TARRAY(NREPLICA))
-         STEP = (HIGHR - LOWR)/DBLE(NREPLICA-1)
-         DO J=0,NREPLICA-1
-            TARRAY(J+1) = LOWR + J*STEP
-         END DO
          IF (REXMODE.EQ."T") THEN
             IF (TASKID.EQ.0) THEN
                TEMP = TARRAY(1)
                DO J=2,NREPLICA
                   CALL MPI_SEND(TARRAY(J),1,MPI_DOUBLE,J-1,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
                END DO
-            ELSE 
-               CALL MPI_REC(TEMP,1,MPI_DOUBLE,0,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
+            ELSE
+               CALL MPI_RECV(TEMP,1,MPI_DOUBLE,0,REMD_TAG,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
             END IF
          END IF
          IF (REXMODE.EQ."H") THEN
@@ -44,12 +57,12 @@ MODULE MD_MPI
                DO J=2,NREPLICA
                   CALL MPI_SEND(TARRAY(J),1,MPI_DOUBLE,J-1,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
                END DO
-            ELSE 
-               CALL MPI_REC(LAMBDA,1,MPI_DOUBLE,0,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
+            ELSE
+               CALL MPI_RECV(LAMBDA,1,MPI_DOUBLE,0,REMD_TAG,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
             END IF
             CALL MPI_BCAST(TEMP,1,MPI_DOUBLE,0,MPI_COMM_WORLD,ERR_CODE_MPI)
-         END IF           
+         END IF
+#endif
       END SUBROUTINE COMMUNICATE_SETTINGS
-
 
 END MODULE MD_MPI
