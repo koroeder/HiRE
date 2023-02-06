abc12032dee8a4e33891200515f12359016d9b8e
kr366 <kr366@cam.ac.uk>
2023-02-06 08:44:57 +0000

    Added H-REX exchange subroutine

--- MD_HiRE/source/REX_exchanges.F90
+++ MD_HiRE/source/REX_exchanges.F90
@@ -91,9 +91,8 @@ MODULE EXCHANGES
                WRITE(MYUNIT, '(A)') " rexmd> Attempting T-REX exchange"
                CALL PASS_DATA_FOR_EXCHANGE_T(MEINITIATET,EXCHANGEDT)
             ELSE IF (REXMODE.EQ.'H') THEN
-               WRITE(*,*) "H_REX not implemented"
-               STOP
-               !CALL PASS_DATA_FOR_EXCHANGE_H(MEINITIATET,EXCHANGEDT)
+               WRITE(MYUNIT, '(A)') " rexmd> Attempting H-REX exchange"
+               CALL PASS_DATA_FOR_EXCHANGE_H(MEINITIATET,EXCHANGEDT)
             END IF
          END IF
 
@@ -110,15 +109,6 @@ MODULE EXCHANGES
          ELSE
             CALL MPI_SEND(EXCHANGEDT,1,MPI_LOGICAL,0,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
          END IF
-         ! TODO: add arguments to MPI barrier - likely need to add MPI COMM RANK calls in thsi routine and in PASS_DATA ...
-         ! TODO: write subroutine to H-REX data passing
-         ! TODO: add pre-compiler statements and change logging and setup in init routines
-         ! TODO: add current_order container to init - should be just the taskids to start
-         ! TODO: need to add additional definitions for MPI variables
-         ! TODO: add debug printing to test routines
-         ! TODO: add exchange steps into MD simulation workflow
-         ! TODO: add termination for MD and error handling for replicas into simulation (can we gracefully terminate?)
-         ! TODO: in init, assign MYCURRENTID from this module
          CALL MPI_BARRIER(MPI_COMM_WORLD,ERR_CODE_MPI)
 #endif
       END SUBROUTINE SELECT_EXCHANGES
@@ -132,7 +122,7 @@ MODULE EXCHANGES
          INCLUDE 'mpif.h'
          INTEGER MPISTATUS(MPI_STATUS_SIZE)
          REAL(KIND=REAL64) :: U11, U12, U21, U22, T1, T2, L1, L2, PROB, DUMMY
-         REAL(KIND=REAL64) :: X1(3*NATOMS), X2(3*NATOMS)
+         REAL(KIND=REAL64) :: X1(3*NATOMS), X2(3*NATOMS), G(3*NATOMS)
          INTEGER :: OTHERREP, J
          INTEGER :: ERR_CODE_MPI, REMD_TAG
          LOGICAL :: SWITCHT
@@ -140,15 +130,84 @@ MODULE EXCHANGES
          ! need to get lambda (L1,L2) from both and coords (X1,X2) from both
          ! set scaling to L1 and get energies for X1 and X2 (U11 and U12)
          ! set scaling to L2 and get energies for X1 and X2 (U21 and U22)
+         ! the initiator receives the information needed for the acceptance/rejection criterion
+         IF (MEINITIATET) THEN
+            OTHERREP = CURRENT_ORDER(MYCURRENTID+1)
+            L1 = LAMBDA
+            X1(1:3*NATOMS) = COORDS(1:3*NATOMS)
+            T1 = TEMP
+            ! set unique tags for each transfer
+            REMD_TAG = (TASKID+1)*1000
+            CALL MPI_RECV(L2,1,MPI_DOUBLE,OTHERREP,REMD_TAG,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
+            CALL MPI_RECV(T2,1,MPI_DOUBLE,OTHERREP,REMD_TAG+1,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
+            CALL MPI_RECV(X2,3*NATOMS,MPI_DOUBLE,OTHERREP,REMD_TAG+2,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
+            CALL MPI_SEND(LAMBDA,1,MPI_DOUBLE,OTHERREP,REMD_TAG+3,MPI_COMM_WORLD,ERR_CODE_MPI)            
+         ELSE
+            OTHERREP = CURRENT_ORDER(MYCURRENTID-1)
+            L2 = LAMBDA
+            ! set the same tag as the receiver
+            REMD_TAG = (OTHERREP+1)*1000
+            CALL MPI_SEND(LAMBDA,1,MPI_DOUBLE,OTHERREP,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
+            CALL MPI_SEND(TEMP,1,MPI_DOUBLE,OTHERREP,REMD_TAG+1,MPI_COMM_WORLD,ERR_CODE_MPI)
+            CALL MPI_SEND(COORDS,3*NATOMS,MPI_DOUBLE,OTHERREP,REMD_TAG+2,MPI_COMM_WORLD,ERR_CODE_MPI)
+            CALL MPI_RECV(L1,1,MPI_DOUBLE,OTHERREP,REMD_TAG+3,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
+         END IF
+         CALL MPI_BARRIER(MPI_COMM_WORLD,ERR_CODE_MPI)
+         WRITE(MYUNIT,'(A)') " passdataH> Sent and received data to determine exchanges" 
 
          ! calculate the probability and apply the acceptance/rejection criterion
-
+         IF (MEINITIATET) THEN
+            ! current scaling set is L1
+            CALL HIRE_ENERGY_GRAD(3*NATOMS, X1, U11, G)
+            CALL HIRE_ENERGY_GRAD(3*NATOMS, X2, U12, G)
+            CALL SET_UNIV_SCALING(L2)
+            CALL HIRE_ENERGY_GRAD(3*NATOMS, X1, U21, G)
+            CALL HIRE_ENERGY_GRAD(3*NATOMS, X2, U22, G)
+            ! the exchnge probability is given by:
+            !H-REX: P(1<->2) = min(1, exp[(1/kT1 - 1/kT2){(U1(x2)-U1(x1)) + (U2(x1)-U2(x2))}])
+            DUMMY = (1.0/T1 - 1.0/T2)*((U12 - U11) + (U21 - U22))
+            PROB = MIN(1.0,EXP(DUMMY))
+            RAND = DPRAND()
+            WRITE(*,*) " rex> Exchanging ", TASKID, " with ", OTHERREP, &
+                       "      DUMMY: ", DUMMY, " ,EXP(DUMMY): ", EXP(DUMMY), &
+                       "      Porb: ", PROB, "and random number: ", RAND
+            ! accept exchange
+            IF (RAND.LT.PROB) THEN
+               SWITCHT = .TRUE.
+            ! or reject it
+            ELSE
+               SWITCHT = .FALSE.
+            END IF
+            ! then pass the status to the other replica (the tags are still set
+            ! and should work as there is an MPI_Barrier before this section)
+            CALL MPI_SEND(SWITCHT,1,MPI_LOGICAL,OTHERREP,REMD_TAG,MPI_COMM_WORLD,ERR_CODE_MPI)
+         ELSE
+            CALL MPI_RECV(SWITCHT,1,MPI_LOGICAL,OTHERREP,REMD_TAG,MPI_COMM_WORLD,MPISTATUS,ERR_CODE_MPI)
+         END IF
          ! if accepted:
          ! exchange lambdas and update the order as for T-REX
-
-         ! if rejected:
+         IF (SWITCHT) THEN
+            WRITE(MYUNIT,'(A)') " passdataH> exchanging replicas"
+            EXCHANGEDT = .TRUE.
+            IF (MEINITIATET) THEN
+               LAMBDA = L2
+            ELSE
+               LAMBDA = L1
+            END IF
+            IF (MEINITIATET) THEN
+               WRITE(*,*) " H_REX> exchanged replicas ", MYCURRENTID, " and ", MYCURRENTID+1 , " with lambda ", L1, " and ", L2
+               MYCURRENTID = MYCURRENTID + 1
+            ELSE 
+               MYCURRENTID = MYCURRENTID - 1
+            END IF
+         ELSE
+            WRITE(MYUNIT,'(A)') " passdataH> not exchanging replicas"
+            EXCHANGEDT = .FALSE.
+         END IF
+         WRITE(MYUNIT,'(A)') " "
+         
          ! make sure to reset scaling
-
+         CALL SET_UNIV_SCALING(LAMBDA)
 #endif 
       END SUBROUTINE PASS_DATA_FOR_EXCHANGE_H
 
@@ -258,45 +317,5 @@ MODULE EXCHANGES
          CALL MPI_BCAST(CURRENT_ORDER,NREPLICA,MPI_INTEGER,0,MPI_COMM_WORLD,ERR_CODE_MPI)
 #endif
       END SUBROUTINE UPDATE_CURR_ORDER
-! get dedicated MPI function for everything - this will make things easier
-
-! key question: keep process id with same replica or keep T in order
-
-! will keep process id with same replica -> book keeping needed (order o T for all replicas)
-! allows to keep log files associated with each individual node
-! on eschange - only need to exchange Ts and kambdas
-
-! what needs to be exhanged:
-! 1. set odd or even exchange step (can use an overall counter rather than logical ...)
-! 2. decide on all pairs to be tested for exchange
-! 3. send all replicas that might be exchanged a signal
-! 4. now send all involved replicas a signal if they are the initiator (lower T/higher lambda)
-! 5. a) initiator receive:
-!      - T2 and U2 for T-REX      
-!      - T2, x2 and lambda for H-REX
-!    b) initiator check acceptance for T-REX
-!    c) for H-REX: calculate U1 and U2 with each x1 and x2, then calculate acceptance
-! 6. initiator send result to head
-! 7. head call exchange of replicas
-
-! exchange:
-! for T-REX
-! make higher and lower T available to both replicas
-! exchange velocities and coordinates
-! scale velocities to new T (see scaling for initiation, sqrt of Tnew/Told)
-
-! for H-REX
-! make lambda available to both
-! exchange velocities and coordinates
-! is there a need to rescle velocities? Or will they be auto adjusted? Looks like no adjustment is done here inn the old OPEP/HiRE simulateur
-
-
-!! Acceptance/rejection
-!
-!T-REX: P(1<->2) = min(1, exp[(1/kT1 - 1/kT2)(U1 - U2)]), where Ui is the potential energy of i
-!
-!H-REX: P(1<->2) = min(1, exp[(1/kT1 - 1/kT2){(U1(x2)-U1(x1)) + (U2(x1)-U2(x2))}])
-!
-
 
 END MODULE EXCHANGES
