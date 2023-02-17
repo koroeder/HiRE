3cb3f971370ab08b2da5a8c908e75c75cd09ba21
kr366 <kr366@cam.ac.uk>
2023-02-17 09:34:01 +0000

    New way of calculating the Debye Hueckel contribution to the energy. Instead of iterating over all atoms, we maintain a list of charged particles. For simulations without protonation changes, this list only needs to be created once and then significantly reduces compute time. For titration simualtions, the list needs to be updated. The new subroutine creating the list can be called multiple times (it de- and reallocates as necessary), and relies on the charges set in CHATM).

--- HiRE_lib/mod_debyehueckel.F90
+++ HiRE_lib/mod_debyehueckel.F90
@@ -9,15 +9,52 @@ MODULE MOD_DEBYEHUECKEL
    !> Debye length
    REAL(KIND = REAL64) :: DL  
    !> Dielectric constant
-   REAL(KIND = REAL64) :: DIEL 
-    
+   REAL(KIND = REAL64) :: DIEL
+   !> Charged particles ids
+   INTEGER, ALLOCATABLE :: CHARGED_IDS(:)
+   !> NUmber of charged particles
+   INTEGER :: NCHARGED = 0
    CONTAINS
       !> Routine to allocate all required arrays
       SUBROUTINE INIT_DH()
          USE NAPARAMS, ONLY: SCORE_RNA
          DIEL = SCORE_RNA(12)
          DL = SCORE_RNA(13)  
+         CALL FIND_CHARGED_PARTICLES()
       END SUBROUTINE INIT_DH
+
+      !> Routine to set array of ids for charged particles (the routine can be called at any time)
+      SUBROUTINE FIND_CHARGED_PARTICLES()
+         USE VAR_DEFS, ONLY: CHATM, NPARTICLES
+         IMPLICIT NONE
+         INTEGER :: IDX
+         INTEGER :: I
+         REAL(KIND = REAL64), PARAMETER :: EPSZERO = 1.0D-6
+
+         CALL DEALLOC_DH()
+         ! first find all charged particles
+         NCHARGED = 0
+         DO I=1,NPARTICLES
+            IF (ABS(CHATM(I)).GT.EPSZERO) THEN
+               NCHARGED = NCHARGED + 1
+            END IF
+         END DO
+         ALLOCATE(CHARGED_IDS(NCHARGED))
+         ! now add the indices to the array
+         IDX = 0
+         DO I=1,NPARTICLES
+            IF (ABS(CHATM(I)).GT.EPSZERO) THEN
+               IDX = IDX + 1
+               CHARGED_IDS(IDX) = I
+            END IF
+         END DO
+         !WRITE(*,*) " find_charged> Found ", NCHARGED, " particles"
+      END SUBROUTINE FIND_CHARGED_PARTICLES
+
+      SUBROUTINE DEALLOC_DH()
+         IMPLICIT NONE
+         IF (ALLOCATED(CHARGED_IDS)) DEALLOCATE(CHARGED_IDS)
+      END SUBROUTINE DEALLOC_DH
 
       !> Calculate the energy and force contribution from the Debye-Hueckel terms
       SUBROUTINE ENERGY_DH(NOPT, X, F, EDH)
@@ -89,7 +126,69 @@ MODULE MOD_DEBYEHUECKEL
          CLOSE(DHUNIT)
 #endif
       END SUBROUTINE ENERGY_DH
-  
+
+      !> Subroutine only iterating over charged particles, increasing computational efficiency
+      SUBROUTINE DH_ENERGY(NOPT, X, F, EDH)
+         USE VAR_DEFS, ONLY: CHATM
+         USE NAPARAMS, ONLY: DHCUT
+#if FOR_ANALYSIS        
+         USE UTILS_IO, ONLY: GETUNIT
+#endif         
+         IMPLICIT NONE
+     
+         INTEGER, INTENT(IN) :: NOPT                   !should be 3*NATOMS
+         REAL(KIND = REAL64), INTENT(IN) :: X(NOPT)    !input coordinates
+         REAL(KIND = REAL64), INTENT(OUT) :: F(NOPT)   !force from bonds
+         REAL(KIND = REAL64), INTENT(OUT) :: EDH
+
+         INTEGER :: I, J, IDX, JDX
+         REAL(KIND = REAL64) :: RIJ(3), R, CHRGI, CHRGJ, EDHPAIR, DFPAIR, NB, DX(3)
+
+#if FOR_ANALYSIS
+         INTEGER :: DHUNIT
+
+         DHUNIT = GETUNIT()
+         OPEN(DHUNIT, FILE="Dbg_DebyeHueckel.dat", STATUS='UNKNOWN')
+#endif
+
+         EDH = 0.0D0
+         F(1:NOPT) = 0.0D0
+         
+         ! iterate over the charged particle list
+         DO IDX=1,NCHARGED-1
+            DO JDX=IDX+1,NCHARGED
+               I = CHARGED_IDS(IDX)
+               J = CHARGED_IDS(JDX)
+               ! get charges
+               CHRGI = CHATM(I)
+               CHRGJ = CHATM(J)
+               ! calculate the distance between the atoms
+               RIJ(1:3) = X(I*3-2:I*3) - X(3*J-2:3*J)
+               R = DSQRT(DOT_PRODUCT(RIJ,RIJ))
+               ! use cutoff here, at 35 Angstrom the contributions are smaller than 10^-6
+               ! using cutoffs of 50 Angstorm and larger seem to work well cumulatively
+               IF (R.LT.DHCUT) THEN
+                  ! get the energy for this pair
+                  EDHPAIR = 0.0D0
+                  DFPAIR = 0.0D0
+                  CALL DH_PAIR(R, EDHPAIR, DFPAIR, CHRGI, CHRGJ)
+                  NB = GET_NBCOEF(I,J) 
+                  EDH = EDH + EDHPAIR * NB               
+                  DX(1:3) = DFPAIR * NB * RIJ(1:3)
+                  !WRITE(*,'(2I8,4F15.7)') I, J, R, EDHPAIR, NB, EDHPAIR*NB
+#ifdef FOR_ANALYSIS
+                  WRITE(DHUNIT,'(4I6,2F7.3,4F15.7)') K, L, I, J, CHRGI, CHRGJ, R, NB, EDHPAIR, EDHPAIR*NB
+#endif
+                  F((I*3-2):I*3) = F((I*3-2):I*3) - DX(1:3)
+                  F((J*3-2):J*3) = F((J*3-2):J*3) + DX(1:3)    
+               END IF           
+            END DO
+         END DO
+#ifdef FOR_ANALYSIS
+         CLOSE(DHUNIT)
+#endif
+      END SUBROUTINE DH_ENERGY
+
       !> DH contribution for a pair of CG particles
       SUBROUTINE DH_PAIR(R, EDH, DF, QI, QJ)
          REAL(KIND = REAL64), INTENT(IN) :: R      ! distance between i and j
