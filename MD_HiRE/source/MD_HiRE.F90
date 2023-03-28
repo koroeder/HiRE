3dead013e231646a8112c997df9d1e0ee3c24e4d
k2262470 <konstantin.roeder@kcl.ac.uk>
2023-03-28 16:53:11 +0100

    Revert "started work on interactive simulations"
    This reverts commit ac96bfb72c1d9833aa52a18bdeb88a9556d4108f.

--- MD_HiRE/source/MD_HiRE.F90
+++ MD_HiRE/source/MD_HiRE.F90
@@ -11,15 +11,12 @@ PROGRAM MD_HIRE
    USE MD_SETUP, ONLY: READ_SETTINGS, SETUP_POTENTIAL, START_TRACKING
    USE MD_SIMULATION
    USE MD_UTILS, ONLY: REPORT_PARAMS, MD_START
-   USE MOD_IMD, ONLY: IMDT
-#ifdef IMD
-   USE MOD_IMD, ONLY: IMD_SETUP
-#endif
 #ifdef MPI
    USE MPI_UTILS, ONLY: COMMUNICATE_SETTINGS, REPORT_PARAMS_MPI, START_TRACKING_MPI
 #endif
    IMPLICIT NONE
    REAL(KIND=REAL64) :: TSTART, TEND, TSETUP, TEQ, TRUNS
+
 #ifdef MPI
    INCLUDE 'mpif.h'
    INTEGER :: ERROR
@@ -32,6 +29,7 @@ PROGRAM MD_HIRE
    TASKID = 0
 #endif
 
+
    CALL CPU_TIME(TSTART)
    ! 1. Simulation setup
    ! a) check the parameter input exists
@@ -39,10 +37,6 @@ PROGRAM MD_HIRE
    ! b) Read in all the simulation settings
    WRITE(MYUNIT,'(A)') " mdhire> Starting setup"
    CALL READ_SETTINGS()
-#ifdef IMD
-   ! setup for interactive simulations
-   CALL IMD_SETUP()
-#endif
    ! communicate the variables
 #ifdef MPI
    CALL COMMUNICATE_SETTINGS()
