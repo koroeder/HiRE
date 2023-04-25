cefdabc7ef0177332c1a1a6f5fd0b4d395af2048
kr366 <konstantin.roeder@kcl.ac.uk>
2023-04-25 16:13:06 +0100

    Added code changes from Samuela: for 81 parameters enabling more flexibility for base pairing, and fixing a bug in the assignment of base types (A and C were swapped)

--- HiRE_lib/fill_params.f90
+++ HiRE_lib/fill_params.f90
@@ -25,7 +25,10 @@ MODULE FILL_PARAMS
          USE VAR_DEFS, ONLY: NRES, RESFINAL, RESNAMES, RESTYPES
          USE UTILS_IO, ONLY: GETUNIT
          USE NAparams, ONLY: WC, WCCanonic, noWC, TIT, noWCq, SCORE_RNA, &
-                           BLIST, BTYPE, BPROT, BP_CURR
+                           BLIST, BTYPE, BPROT, BP_CURR, &
+                           cwwAA, cwwAG, cwwAC, cwwAU, cwwGC, cwwGU, cwwCC, cwwCU, cwwUU, &
+                           twwAA, twwAC, twwAU, twwGG, twwGU, twwGC, twwCC, twwCU, twwUU, &
+                           cwh, twh, cws, tws, chh, thh, chs, ths, css, tss
          USE RNA_HB_PARAMS, ONLY: FILL_RNA_HB_PARAMS
          USE DNA_HB_PARAMS, ONLY: FILL_DNA_HB_PARAMS
        
@@ -38,6 +41,38 @@ MODULE FILL_PARAMS
          noWC = SCORE_RNA(41)
          TIT = SCORE_RNA(42)
          noWCq = SCORE_RNA(43)
+         
+         cwwAA = SCORE_RNA(54)*SCORE_RNA(39)
+         cwwAG = SCORE_RNA(55)*SCORE_RNA(39)
+         cwwAC = SCORE_RNA(56)*SCORE_RNA(39)
+         cwwAU = SCORE_RNA(57)*SCORE_RNA(40)
+         cwwGC = SCORE_RNA(58)*SCORE_RNA(39)
+         cwwGU = SCORE_RNA(59)*SCORE_RNA(40)
+         cwwCC = SCORE_RNA(60)*SCORE_RNA(39)
+         cwwCU = SCORE_RNA(61)*SCORE_RNA(39)
+         cwwUU = SCORE_RNA(62)*SCORE_RNA(39)
+         
+         twwAA = SCORE_RNA(63)*SCORE_RNA(39)
+         twwAC = SCORE_RNA(64)*SCORE_RNA(39)
+         twwAU = SCORE_RNA(65)*SCORE_RNA(39)
+         twwGG = SCORE_RNA(66)*SCORE_RNA(39)
+         twwGC = SCORE_RNA(67)*SCORE_RNA(39)
+         twwGU = SCORE_RNA(68)*SCORE_RNA(39)
+         twwCC = SCORE_RNA(69)*SCORE_RNA(39)
+         twwCU = SCORE_RNA(70)*SCORE_RNA(39)
+         twwUU = SCORE_RNA(71)*SCORE_RNA(39)
+         
+         cwh = SCORE_RNA(72)*SCORE_RNA(41)
+         twh = SCORE_RNA(73)*SCORE_RNA(41)
+         cws = SCORE_RNA(74)*SCORE_RNA(41)
+         tws = SCORE_RNA(75)*SCORE_RNA(41)
+         chh = SCORE_RNA(76)*SCORE_RNA(41)
+         thh = SCORE_RNA(77)*SCORE_RNA(41)
+         chs = SCORE_RNA(78)*SCORE_RNA(41)
+         ths = SCORE_RNA(79)*SCORE_RNA(41)
+         css = SCORE_RNA(80)*SCORE_RNA(41)
+         tss = SCORE_RNA(81)*SCORE_RNA(41)
+
          
          !fill base information
          CALL ALLOC_NAPARAMS(NRES)
@@ -53,10 +88,10 @@ MODULE FILL_PARAMS
                BTYPE(I) = 1
                RESTYPES(I) = 0
             ELSEIF (RESNAMES(I)(1:1).EQ."C") THEN 
-               BTYPE(I) = 2
+               BTYPE(I) = 3
                RESTYPES(I) = 0
             ELSEIF (RESNAMES(I)(1:1).EQ."A") THEN 
-               BTYPE(I) = 3
+               BTYPE(I) = 2
                RESTYPES(I) = 0
             ELSEIF (RESNAMES(I)(1:1).EQ."U") THEN
                BTYPE(I) = 4
