922919b95b68ef7db0774d777762673c708af34d
kr366 (k2262470) <kr366@cam.ac.uk>
2023-06-13 16:20:12 +0100

    Updated number of parameters

--- HiRE_lib/NAparams.f90
+++ HiRE_lib/NAparams.f90
@@ -7,7 +7,7 @@ MODULE NAparams
    IMPLICIT NONE
 
    !> number of parameters given in scale.dat
-   INTEGER, PARAMETER :: SCORESIZE=81
+   INTEGER, PARAMETER :: SCORESIZE=79
    !> HiRE potential parameters given in scale.dat
    REAL(KIND = REAL64) :: SCORE_RNA(SCORESIZE)
    
