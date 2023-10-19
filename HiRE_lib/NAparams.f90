2ff987a287b2136ff3a99198df731412bbce734b
kr366 <konstantin.roeder@kcl.ac.uk>
2023-10-19 16:28:06 +0100

    Bug fix: updated number of paramters

--- HiRE_lib/NAparams.f90
+++ HiRE_lib/NAparams.f90
@@ -7,7 +7,7 @@ MODULE NAparams
    IMPLICIT NONE
 
    !> number of parameters given in scale.dat
-   INTEGER, PARAMETER :: SCORESIZE=98 
+   INTEGER, PARAMETER :: SCORESIZE=99
    !> HiRE potential parameters given in scale.dat
    REAL(KIND = REAL64) :: SCORE_RNA(SCORESIZE)
    
