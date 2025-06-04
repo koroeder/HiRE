76dfe5cf271d3496de4f20e4b5e2207140319d11
k2262470 <konstantin.roeder@kcl.ac.uk>
2025-06-04 08:57:55 +0100

    Updated particle numbers and names to reflect change from C5 to O3 and separating C4 for RNA (R4) and DNA (S4)

--- HiRE_lib/mod_nbdef.f90
+++ HiRE_lib/mod_nbdef.f90
@@ -9,11 +9,11 @@ MODULE NBDEFS
    ! NTYPES is the number of particle types, found in the .top file
    ! While the number can potentially change, we have 17 types implemented 
    ! Currently, it's taken to be :
-   ! 1: C5*  2: O5*  3: P  4: CA  5: CY (RNA), 6: CY (DNA),
-   ! 7,8: G1,2  9,10: A1,2  11: U1  12: C1, 13: T1,
-   ! 14: D 15: MG 16: NA  17: CL
+   ! 1: O3'  2: O5'  3: P  4: R4 (RNA C4') 5: S4 (RNA C4') 6: R1 (RNA C1'), 7: S1 (DNA C1'),
+   ! 8,9: G1,2  10,11: A1,2  12: U1  13: C1, 14: T1,
+   ! 15: D 16: MG 17: NA  18: CL
    !> Number of different particles currently implemented
-   INTEGER, PARAMETER  :: NTYPES = 17              
+   INTEGER, PARAMETER  :: NTYPES = 18              
    !> Size paramters for general beads
    REAL(KIND = REAL64) :: NBCT2GEN   = 3.6D0
    !> Size paramters for C4 beads   
@@ -41,7 +41,7 @@ MODULE NBDEFS
    REAL(KIND = REAL64), DIMENSION(NTYPES), PARAMETER ::  &
                          CHRG = (/0.0, 0.0,-1.0, 0.0, 0.0, 0.0, &
                                   0.0, 0.0, 0.0, 0.0, 0.0, 0.0, &
-                                  0.0, 0.0, 2.0, 1.0,-1.0 /) 
+                                  0.0, 0.0, 0.0, 2.0, 1.0,-1.0 /) 
                                                
    CONTAINS
       !> Routine to populate all NB arrays
@@ -54,26 +54,28 @@ MODULE NBDEFS
          ! C4 are bigger
          NBCT2(4,1:NTYPES) = NBCT2C4
          NBCT2(1:NTYPES,4) = NBCT2C4
+         NBCT2(5,1:NTYPES) = NBCT2C4
+         NBCT2(1:NTYPES,5) = NBCT2C4       
          ! CY are bigger - not used
-         NBCT2(5,1:NTYPES) = NBCT2CY      
-         NBCT2(1:NTYPES,5) = NBCT2CY
          NBCT2(6,1:NTYPES) = NBCT2CY      
-         NBCT2(1:NTYPES,6) = NBCT2CY      
+         NBCT2(1:NTYPES,6) = NBCT2CY
+         NBCT2(7,1:NTYPES) = NBCT2CY      
+         NBCT2(1:NTYPES,7) = NBCT2CY      
          ! bases beads size 
-         NBCT2(7:13,7:13) = NBCT2BASE
+         NBCT2(8:14,8:14) = NBCT2BASE
          ! phosphorus size       
          NBCT2(3,3) = NBCT2PP
          ! Dummy particles
-         NBCT2(14,1:NTYPES) = NBCT2DUMMY
-         NBCT2(1:NTYPES,14) = NBCT2DUMMY
+         NBCT2(15,1:NTYPES) = NBCT2DUMMY
+         NBCT2(1:NTYPES,15) = NBCT2DUMMY
          !effective radius adjustments
          ! P-P  and Mg++, Na+ and Cl- interactions
-         NBSCORE(3,15:17) = NBRADLARGE ! P-ion interactions
-         NBSCORE(15:17,3) = NBRADLARGE ! P-ion interactions
-         NBSCORE(15,16:17) = NBRADLARGE ! Mg - Na,Cl interactions
-         NBSCORE(16:17,15) = NBRADLARGE ! Na,Cl - Mg interactions
+         NBSCORE(3,16:18) = NBRADLARGE ! P-ion interactions
+         NBSCORE(16:18,3) = NBRADLARGE ! P-ion interactions
+         NBSCORE(16,17:18) = NBRADLARGE ! Mg - Na,Cl interactions
+         NBSCORE(17:18,16) = NBRADLARGE ! Na,Cl - Mg interactions
          NBSCORE(3,3) = NBRADLARGE   ! P-P interactions 
-         NBSCORE(15,15) = NBRADLARGE ! Mg++ interactions      
+         NBSCORE(16,16) = NBRADLARGE ! Mg++ interactions      
       END SUBROUTINE SET_NBPARAMS
  
       !> Routine to populate all NB arrays
