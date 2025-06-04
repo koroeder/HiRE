4e9ad66ed3c67fee082b5209e3681c17c21b0e82
k2262470 <konstantin.roeder@kcl.ac.uk>
2025-06-04 08:59:55 +0100

    Added quartic angle potential for those angular terms with a double well type potential based on sugar puckering. Removed the theta typing for the square type potential as we will use fixed force constants to get the relative energies and then only scale the overall energy

--- HiRE_lib/mod_angles.f90
+++ HiRE_lib/mod_angles.f90
@@ -10,13 +10,18 @@ MODULE MOD_ANGLES
    !> Number of unique angle types
    INTEGER :: NUMANG   
 
+   !> Number of angles
+   INTEGER :: NQANGLES  
+   !> Number of unique angle types
+   INTEGER :: NUMQANG   
+
    !Angles: E = TK * (T - TEQ)**2 with T the current angle
    !> Angular force constants
    REAL(KIND = REAL64), ALLOCATABLE :: TK(:)     
    !> Equilibrium angle  
    REAL(KIND = REAL64), ALLOCATABLE :: TEQ(:)  
   
-   !book-keeping for angles
+   !book-keeping for angles - square potential (one equilibrium angle)
    !> Angle atom1
    INTEGER, ALLOCATABLE :: IT(:)                 
    !> Angle atom2
@@ -24,139 +29,59 @@ MODULE MOD_ANGLES
    !> Angle atom3
    INTEGER, ALLOCATABLE :: KT(:)                 
    !> Type of angle
-   INTEGER, ALLOCATABLE :: ICT(:)                
+   INTEGER, ALLOCATABLE :: ICT(:)  
+   
+   !Quartic angles
+   ! "force" constant of overall energy (effectively scaling)
+   REAL(KIND = REAL64), ALLOCATABLE :: TKQ(:)
+   ! Equilibrium angles (correpsond to the maximum in the quartic potential)
+   REAL(KIND = REAL64), ALLOCATABLE :: REFQ(:)
 
-   !> Angle type information needed for CG scaling
-   INTEGER, ALLOCATABLE :: NTHETATYPE(:)
+   !book keeping
+   !> Angle atom 1
+   INTEGER, ALLOCATABLE :: ITQ(:)                 
+   !> Angle atom2
+   INTEGER, ALLOCATABLE :: JTQ(:)                 
+   !> Angle atom3
+   INTEGER, ALLOCATABLE :: KTQ(:)  
+   !> Type of qangle
+   INTEGER, ALLOCATABLE :: ICTQ(:)  
+
+   !potential parameters
+   REAL(KIND = REAL64), ALLOCATABLE :: AQ(:), BQ(:), CQ(:), DQ(:), EQ(:)
 
    CONTAINS
       !> Routine to allocate all required arrays
       SUBROUTINE ALLOC_ANGLES()
          CALL DEALLOC_ANGLES()
          ALLOCATE(TK(NUMANG), TEQ(NUMANG), IT(NANGLES), JT(NANGLES), &
-                  KT(NANGLES), ICT(NANGLES), NTHETATYPE(NANGLES))
+                  KT(NANGLES), ICT(NANGLES))
+         ALLOCATE(TKQ(NUMQANG), REFQ(NUMQANG), AQ(NUMQANG), BQ(NUMQANG), &
+                  CQ(NUMQANG), DQ(NUMQANG), EQ(NUMQANG), ITQ(NQANGLES), &
+                  JTQ(NQANGLES), KTQ(NQANGLES), ICTQ(NQANGLES))
       END SUBROUTINE ALLOC_ANGLES  
 
-      !> Assign the angle type to obtain the correct CG scaling information
-      SUBROUTINE ASSIGN_THETATYPE()
-         USE VAR_DEFS, ONLY: IAC, RESTYPES, ATOMINRES_LOOKUP
-         IMPLICIT NONE
-         INTEGER :: JN, I3, J3, K3T, IT0, IT1, IT2, MOLTYPE
+      !> Routine to get all anglar contributions
+      SUBROUTINE ENERGY_ALL_ANGLES(NOPT, X, F, EANGLE)
+         INTEGER, INTENT(IN) :: NOPT                   !should be 3*NATOMS
+         REAL(KIND = REAL64), INTENT(IN) :: X(NOPT)    !input coordinates
+         REAL(KIND = REAL64), INTENT(OUT) :: F(NOPT)   !force from bonds
+         REAL(KIND = REAL64), INTENT(OUT) :: EANGLE  
 
-         ! Atom types (IAC):
-         ! 1: C  2: O  3: P  4: R4  5: R1  6: S1
-         ! 7,8: G1,2  9,10: A1,2  11: U1  12: C1
-         ! 13: T1  14: D  15: MG  16: NA  17:CL
-         ! RNA angle types (Theta type, angle, IACs)
-         ! 0 - R4-R1-X1         4-5-(7,9,11,12)
-         ! 1 - R1-A1/G1-A2/G2   5-(7/9)-(8/10)
-         ! 2 - P-O-C            3-2-1
-         ! 3 - O-C-R4           2-1-4
-         ! 4 - C-R4-P           1-4-3
-         ! 5 - R4-P-O           4-3-2
-         ! 6 - C-R4-R1          1-4-5
-         ! 7 - R1-R4-P          5-4-3
-         ! DNA angle types (Theta type, angle, IACs)
-         ! 8 - R4-S1-X1         4-6-(7,9,12,13)
-         ! 9 - S1-A1/G1-A2/G2   6-(7/9)-(8/10)
-         !10 - P-O-C            3-2-1
-         !11 - O-C-R4           2-1-4
-         !12 - C-R4-P           1-4-3
-         !13 - R4-P-O           4-3-2
-         !14 - C-R4-S1          1-4-6
-         !15 - S1-R4-P          6-4-3    
-         !
-         !QUERY: Potentially we could merge 2 and 10, 3 and 11, 4 and 12 and 5 and 13
-         !       Will they actually be different?     
-         DO JN=1,NANGLES
-            I3 = IT(JN)/3 + 1
-            J3 = JT(JN)/3 + 1
-            K3T = KT(JN)/3 + 1
-            IT0 = IAC(I3)
-            IT1 = IAC(J3)
-            IT2 = IAC(K3T)
-            ! MOLTYPE is coding for RNA (0) or DNA (1)
-            MOLTYPE = RESTYPES(ATOMINRES_LOOKUP(K3T))
+         REAL(KIND = REAL64) :: F1(NOPT), F2(NOPT), E1, E2
 
-            IF (IT0.EQ.1) THEN
-               !IT1=4 ; IT2=3
-               IF (IT2.EQ.3) THEN
-                  IF (MOLTYPE.EQ.0) THEN
-                     NTHETATYPE(JN) = 4
-                  ELSE IF (MOLTYPE.EQ.1) THEN
-                     NTHETATYPE(JN) = 12
-                  END IF
-               !IT1=4 ; IT2=5 
-               ELSE IF (IT2.EQ.5) THEN                  
-                  NTHETATYPE(JN) = 6
-               !IT1=4 ; IT2=6  
-               ELSE IF (IT2.EQ.6) THEN                  
-                  NTHETATYPE(JN) = 14
-               ENDIF
-            ELSE IF (IT0.EQ.2) THEN
-               !IT1=1 ; IT2=4
-               IF (IT1.EQ.1) THEN
-                  IF (MOLTYPE.EQ.0) THEN
-                     NTHETATYPE(JN) = 3
-                  ELSE IF (MOLTYPE.EQ.1) THEN
-                     NTHETATYPE(JN) = 11
-                  END IF
-               ENDIF        
-            ELSE IF (IT0.EQ.3) THEN
-               !IT1=2 ; IT2=1
-               IF (IT1.EQ.2) THEN
-                  IF (MOLTYPE.EQ.0) THEN
-                     NTHETATYPE(JN) = 2
-                  ELSE IF (MOLTYPE.EQ.1) THEN
-                     NTHETATYPE(JN) = 10
-                  END IF                  
-               ENDIF         
-            ELSE IF (IT0.EQ.4) THEN
-               !IT1=5 ; IT2=(7,9,11,12)
-               IF (IT1.EQ.5) THEN
-                  NTHETATYPE(JN) = 0
-               !IT1=6 ; IT2=(7,9,12,13)                 
-               ELSE IF (IT1.EQ.6) THEN
-                  NTHETATYPE(JN) = 8
-               !IT1=3 ; IT2=2
-               ELSE IF (IT1.EQ.3) THEN
-                  IF (MOLTYPE.EQ.0) THEN
-                     NTHETATYPE(JN) = 5
-                  ELSE IF (MOLTYPE.EQ.1) THEN
-                     NTHETATYPE(JN) = 13
-                  END IF           
-               ENDIF         
-            ELSE IF (IT0.EQ.5) THEN
-               !IT1=7 ; IT2=8
-               IF (IT1.EQ.7) THEN         
-                  NTHETATYPE(JN) = 1 
-               !IT1=9 ; IT2=10 
-               ELSE IF (IT1.EQ.9) THEN
-                  NTHETATYPE(JN) = 1 
-               !IT1=4 ; IT2=3   
-               ELSE IF (IT1.EQ.4) THEN
-                  NTHETATYPE(JN) = 7                    
-               ENDIF
-            ELSE IF (IT0.EQ.6) THEN
-               !IT1=7 ; IT2=8
-               IF (IT1.EQ.7) THEN         
-                  NTHETATYPE(JN) = 9 
-               !IT1=9 ; IT2=10 
-               ELSE IF (IT1.EQ.9) THEN
-                  NTHETATYPE(JN) = 9 
-               !IT1=4 ; IT2=3   
-               ELSE IF (IT1.EQ.4) THEN
-                  NTHETATYPE(JN) = 15                    
-               ENDIF               
-            ELSE
-               WRITE(*,*) " assign_thetatype> ERROR: Unkown theta type"
-               STOP
-            ENDIF
-         ENDDO
-      END SUBROUTINE ASSIGN_THETATYPE
+         !initialise force and energy
+         F(1:NOPT) = 0.0d0
+         EANGLE = 0.0d0
+
+         CALL ENERGY_ANGLES(NOPT, X, F1, E1)
+         CALL ENERGY_QANGLES(NOPT, X, F2, E1)
+
+         EANGLE = E1 + E2
+         F(1:NOPT) = F1(1:NOPT) + F2(1:NOPT)
+      END SUBROUTINE ENERGY_ALL_ANGLES
 
       !> Calculate the energy and force contribution from the angular terms
-      !QUERY: Why is there an if TK(IC) .GE. 2.0D0?
       SUBROUTINE ENERGY_ANGLES(NOPT, X, F, EANGLE)
          USE NAPARAMS, ONLY: SCORE_RNA
          USE PREC_HIRE
@@ -179,41 +104,107 @@ MODULE MOD_ANGLES
 
          DO JN = 1,NANGLES
             IC = ICT(JN)
-            IF (TK(IC) .GE. 2.0d0) THEN
-               I = IT(JN)
-               J = JT(JN)
-               K = KT(JN)
-               RIJ = X(I+1:I+3)-X(J+1:J+3)
-               RKJ = X(K+1:K+3)-X(J+1:J+3)
+            I = IT(JN)
+            J = JT(JN)
+            K = KT(JN)
+            RIJ = X(I+1:I+3)-X(J+1:J+3)
+            RKJ = X(K+1:K+3)-X(J+1:J+3)
 
-               RIJ0 = dot_product(RIJ, RIJ)
-               RKJ0 = dot_product(RKJ, RKJ)
-               RIK0 = dsqrt(RIJ0*RKJ0)
-               CT0 = dot_product(RIJ, RKJ)/RIK0
-               ! QUERY: This is clearly for numerical reasons, but it feels like 
-               !        there ought to be a better solution for this using TANH?
-               CT1 = MAX(-PT999,CT0)
-               CT2 = MIN(PT999,CT1)
-               ANT = DACOS(CT2)
+            RIJ0 = dot_product(RIJ, RIJ)
+            RKJ0 = dot_product(RKJ, RKJ)
+            RIK0 = dsqrt(RIJ0*RKJ0)
+            CT0 = dot_product(RIJ, RKJ)/RIK0
+            ! QUERY: This is clearly for numerical reasons, but it feels like 
+            !        there ought to be a better solution for this using TANH?
+            CT1 = MAX(-PT999,CT0)
+            CT2 = MIN(PT999,CT1)
+            ANT = DACOS(CT2)
 
-               ! ENERGY
-               DA = ANT-TEQ(IC)
-               DF = TK(IC)*DA*SCORE_RNA(3+NTHETATYPE(JN))*SCORE_RNA(2)
-               EAW = DF*DA
-               DFW = -(2*DF)/DSIN(ANT)
+            ! ENERGY
+            DA = ANT-TEQ(IC)
+            DF = TK(IC)*DA*SCORE_RNA(2)
+            EAW = DF*DA
+            DFW = -(2*DF)/DSIN(ANT)
 
-               EANGLE = EANGLE + EAW
-               ! FORCE
-               rDI = DFW*(rKJ/RIK0-CT2*rIJ/RIJ0)
-               rDK = DFW*(rIJ/RIK0-CT2*rKJ/RKJ0)
-               rDJ = -rDI-rDK
-               F(I+1:I+3) = F(I+1:I+3) - rDI
-               F(J+1:J+3) = F(J+1:J+3) - rDJ
-               F(K+1:K+3) = F(K+1:K+3) - rDK                 
-            ENDIF
+            EANGLE = EANGLE + EAW
+            ! FORCE
+            rDI = DFW*(rKJ/RIK0-CT2*rIJ/RIJ0)
+            rDK = DFW*(rIJ/RIK0-CT2*rKJ/RKJ0)
+            rDJ = -rDI-rDK
+            F(I+1:I+3) = F(I+1:I+3) - rDI
+            F(J+1:J+3) = F(J+1:J+3) - rDJ
+            F(K+1:K+3) = F(K+1:K+3) - rDK                 
          END DO
 
       END SUBROUTINE ENERGY_ANGLES
+
+      SUBROUTINE ENERGY_QANGLES(NOPT, X, F, EQANGLE)
+         USE NAPARAMS, ONLY: SCORE_RNA
+         USE PREC_HIRE
+         IMPLICIT NONE  
+      
+         INTEGER, INTENT(IN) :: NOPT                   !should be 3*NATOMS
+         REAL(KIND = REAL64), INTENT(IN) :: X(NOPT)    !input coordinates
+         REAL(KIND = REAL64), INTENT(OUT) :: F(NOPT)   !force from bonds
+         REAL(KIND = REAL64), INTENT(OUT) :: EQANGLE  
+
+         REAL(KIND = REAL64) :: RIJ(3), RKJ(3), RIJ0, RKJ0, RIK0
+         REAL(KIND = REAL64) :: RDI(3), RDJ(3), RDK(3)
+         REAL(KIND = REAL64) :: CT0, CT1, CT2, ANT
+         REAL(KIND = REAL64) :: A, B, C, D, E, REF, KANG, DIFF, DIFF2, DIFF3, DIFF4, EQ, DF
+         REAL(KIND = REAL64), PARAMETER :: PT999 = 0.999d0
+         INTEGER :: JN, I, J, K, IC
+
+         !initialise force and energy
+         F(1:NOPT) = 0.0d0
+         EQANGLE = 0.0d0
+
+         DO JN = 1,NQANGLES
+            I = ITQ(JN)
+            J = JTQ(JN)
+            K = KTQ(JN)
+            IC = ICTQ(JN)
+            RIJ = X(I+1:I+3)-X(J+1:J+3)
+            RKJ = X(K+1:K+3)-X(J+1:J+3)
+
+            RIJ0 = dot_product(RIJ, RIJ)
+            RKJ0 = dot_product(RKJ, RKJ)
+            RIK0 = dsqrt(RIJ0*RKJ0)
+            CT0 = dot_product(RIJ, RKJ)/RIK0
+            ! QUERY: This is clearly for numerical reasons, but it feels like 
+            !        there ought to be a better solution for this using TANH?
+            CT1 = MAX(-PT999,CT0)
+            CT2 = MIN(PT999,CT1)
+            ANT = DACOS(CT2)
+
+            !Energy of angle term
+            A = AQ(IC)
+            B = BQ(IC)
+            C = CQ(IC)
+            D = DQ(IC)
+            E = EQ(IC)
+            REF = REFQ(IC)
+            KANG = TKQ(IC)
+
+            DIFF = ANT - REF
+            DIFF2 = DIFF**2
+            DIFF3 = DIFF**3
+            DIFF4 = DIFF**4
+
+            EQ = KANG*(A*DIFF4 + B*DIFF3 + C*DIFF2 + D*DIFF + E)*SCORE_RNA(2)
+            EQANGLE = EQANGLE + EQ
+
+            !Force
+            DF = KANG*(4*A*DIFF3+3*B*DIFF2 + 2*C*DIFF + D)*SCORE_RNA(2) ! derivative of V_qangle with respect to theta
+            ! now need derivative of theta with repsect to x,y,z
+            rDI = DF*(rKJ/RIK0-CT2*rIJ/RIJ0)
+            rDK = DF*(rIJ/RIK0-CT2*rKJ/RKJ0)
+            rDJ = -rDI-rDK
+            F(I+1:I+3) = F(I+1:I+3) - rDI
+            F(J+1:J+3) = F(J+1:J+3) - rDJ
+            F(K+1:K+3) = F(K+1:K+3) - rDK  
+         END DO
+      END SUBROUTINE ENERGY_QANGLES
 
       !> Deallocate all arrays in this module
       SUBROUTINE DEALLOC_ANGLES()
@@ -223,7 +214,17 @@ MODULE MOD_ANGLES
          IF (ALLOCATED(JT)) DEALLOCATE(JT)
          IF (ALLOCATED(KT)) DEALLOCATE(KT)
          IF (ALLOCATED(ICT)) DEALLOCATE(ICT)
-         IF (ALLOCATED(NTHETATYPE)) DEALLOCATE(NTHETATYPE)
+         IF (ALLOCATED(TKQ)) DEALLOCATE(TKQ)
+         IF (ALLOCATED(REFQ)) DEALLOCATE(REFQ)
+         IF (ALLOCATED(AQ)) DEALLOCATE(AQ)
+         IF (ALLOCATED(BQ)) DEALLOCATE(BQ)
+         IF (ALLOCATED(CQ)) DEALLOCATE(CQ)
+         IF (ALLOCATED(DQ)) DEALLOCATE(DQ)
+         IF (ALLOCATED(EQ)) DEALLOCATE(EQ)
+         IF (ALLOCATED(ITQ)) DEALLOCATE(ITQ)
+         IF (ALLOCATED(JTQ)) DEALLOCATE(JTQ)
+         IF (ALLOCATED(KTQ)) DEALLOCATE(KTQ)
+         IF (ALLOCATED(ICTQ)) DEALLOCATE(ICTQ)
       END SUBROUTINE DEALLOC_ANGLES
 
 END MODULE MOD_ANGLES
